a16742f236013be2903182f60e6e60acea74d608
Charles Chen <cyuchen@ucdavis.edu>
2015-10-07 20:13:35 -0700

    update coursera work

--- hw1.m
+++ hw1.m
@@ -1,8 +1,3 @@
-%problem 1
-load('data.txt'); %creates a vector that contains data
-size(data);
-mpg = data(:,1);
-boundary = quantile(mpg, [.33,.66])
 %     1. mpg:           continuous
 %     2. cylinders:     multi-valued discrete
 %     3. displacement:  continuous
@@ -13,8 +8,12 @@ boundary = quantile(mpg, [.33,.66])
 %     8. origin:        multi-valued discrete
 %     9. car name:      string (unique for each instance)
 
-%problem 2
+%problem 1
+load('data.txt');
+mpg = data(:,1);
+boundary = quantile(mpg, [.33,.66]);
 
+%problem 2
 low = (mpg < boundary(1));
 med = 2 * (mpg >= boundary(1) & mpg <= boundary(2));
 high = 3 * (mpg > boundary(2));
@@ -23,3 +22,8 @@ label = low + med + high;
 gplotmatrix(data, [], label);
 
 %problem 3
+result = OLS([ones(392,1), data(:,2)], mpg);
+
+%problem 4
+training = data(1:280, :);
+testing = data(281:392, :)
